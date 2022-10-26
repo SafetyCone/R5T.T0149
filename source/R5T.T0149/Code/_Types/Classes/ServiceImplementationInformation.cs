24266760ef90e335e3230d74790b65a1ffb8572a
davidcoats <david@getsafetycone.com>
2022-10-26 04:19:59 -0700

    Interim changes.

---
+++ source/R5T.T0149/Code/_Types/Classes/ServiceImplementationInformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using R5T.T0142;
+
+
+namespace R5T.T0149
+{
+    /// <summary>
+    /// Information describing a service implementation.
+    /// </summary>
+    [DataTypeMarker]
+    public class ServiceImplementationInformation
+    {
+        public string ImplementationNamespacedTypeName { get; set; }
+        public string DefinitionNamespacedTypeName { get; set; }
+        public string[] DependencyDefinitionNamespacedTypeNames { get; set; }
+    }
+}
